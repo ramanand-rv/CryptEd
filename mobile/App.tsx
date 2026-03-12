7c84b6e0cf713922022d768442a5a01a5deee130
Ramanand Thakur <ramanandt35@gmail.com>
2026-03-12 23:53:25 +0530

    mobile/add tab nav after login

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AuthProvider, useAuth } from "./src/context/AuthContext";
 import { WalletProvider } from "./src/context/WalletContext";
 import LoginScreen from "./src/screens/LoginScreen";
-import HomeScreen from "./src/screens/HomeScreen";
 import CourseListScreen from "./src/screens/CourseListScreen";
 import CourseDetailScreen from "./src/screens/CourseDetailScreen";
 import CoursePlayerScreen from "./src/screens/CoursePlayerScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function AuthStack() {
   return (
@@ -23,14 +25,10 @@ function AuthStack() {
   );
 }
 
-function AppStack() {
+// Stack for Courses tab
+function CoursesStack() {
   return (
-    <Stack.Navigator initialRouteName="CourseList">
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ title: "Dashboard" }}
-      />
+    <Stack.Navigator>
       <Stack.Screen
         name="CourseList"
         component={CourseListScreen}
@@ -46,14 +44,27 @@ function AppStack() {
         component={CoursePlayerScreen}
         options={{ title: "Course Player" }}
       />
-
     </Stack.Navigator>
+  );
+}
+
+// Main app with bottom tabs
+function AppTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Courses"
+        component={CoursesStack}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 }
 
 function AppNavigator() {
   const { user } = useAuth();
-  return user ? <AppStack /> : <AuthStack />;
+  return user ? <AppTabs /> : <AuthStack />;
 }
 
 export default function App() {
